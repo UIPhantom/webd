d3d3e93daa7324fd7e6a948b5cede6d1a867677a
Sachu-D <1822416.bca.cbsa@cgc.edu.in>
2023-04-08 13:58:44 +0530

    dom js added

---
+++ dom.js
@@ -0,0 +1,10 @@
+
+var li = document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[1]);
+li[1].textContent = 'Hello 2';
+li[2].style.backgroundColor = 'green';
+
+for (var i = 0; i < li.length; i++){
+    li[i].style.fontWeight = 'bold';
+}
