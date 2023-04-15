c5a17328403525cee1709b4a86ac9de4bfe8ea25
Sachu-D <1822416.bca.cbsa@cgc.edu.in>
2023-04-15 12:27:10 +0530

    queryselector

--- dom.js
+++ dom.js
@@ -1,10 +1,17 @@
 
-var li = document.getElementsByTagName('li');
-// console.log(li);
-// console.log(li[1]);
-li[1].textContent = 'Hello 2';
-li[2].style.backgroundColor = 'green';
+// var li = document.getElementsByTagName('li');
+// // console.log(li);
+// // console.log(li[1]);
+// li[1].textContent = 'Hello 2';
+// li[2].style.backgroundColor = 'green';
 
-for (var i = 0; i < li.length; i++){
-    li[i].style.fontWeight = 'bold';
-}
+// for (var i = 0; i < li.length; i++){
+//     li[i].style.fontWeight = 'bold';
+// }
+
+//queryselector
+var lastItem = document.querySelector('.list-group-item:nth-child(2)');
+lastItem.style.backgroundColor = "green";
+
+var secondItem = document.querySelector('.list-group-item:nth-child(3)');
+secondItem.style.display = "none";
