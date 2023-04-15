2543cf3e1a08812f94c301e0b26ddcaebf365365
Sachu-D <1822416.bca.cbsa@cgc.edu.in>
2023-04-15 12:57:44 +0530

    queryselectorAll

--- dom.js
+++ dom.js
@@ -9,9 +9,18 @@
 //     li[i].style.fontWeight = 'bold';
 // }
 
-//queryselector
-var lastItem = document.querySelector('.list-group-item:nth-child(2)');
-lastItem.style.backgroundColor = "green";
+// //queryselector
+// var lastItem = document.querySelector('.list-group-item:nth-child(2)');
+// lastItem.style.backgroundColor = "green";
 
-var secondItem = document.querySelector('.list-group-item:nth-child(3)');
-secondItem.style.display = "none";
+// var secondItem = document.querySelector('.list-group-item:nth-child(3)');
+// secondItem.style.display = "none";
+
+//queryselectorAll
+var items = document.querySelectorAll('li:nth-child(2)');
+items[0].style.color = 'green';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+for (var i = 0; i < odd.length; i++){
+    odd[i].style.backgroundColor = "green";
+}
